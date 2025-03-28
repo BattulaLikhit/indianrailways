3d4cef92c6a64fab138483e1db8698b1d2180154
BattulaLikhit <Battula.Likhit@cognizant.com>
2025-03-28 15:11:08 +0530

    First Push

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/railway")
+public class DemoController {
+       @GetMapping
+       public String get() {
+    	   return "Hello Bro!";
+       }
+       @GetMapping("/welcome")
+       public String getWelcome() {
+    	   return "Welcome bro!";
+       }
+}
